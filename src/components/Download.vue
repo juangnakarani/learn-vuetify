c1fb6d5bc3cc3ac756e984347ddf479fb437d18a
juangnakarani <juang.nakarani@gmail.com>
2017-10-13 18:33:47 +0700

    add filter upload size

--- src/components/Download.vue
+++ src/components/Download.vue
@@ -1,19 +1,75 @@
 <template>
-    <v-container fuild>
-        <v-layout justify-center>
-            
-            <v-card color="green" dark>
-              <v-card-text 
-              v-text="lorem">
-              </v-card-text>
-            </v-card>
-        </v-layout>
-    </v-container>
+  <v-container grid-list-md>
+    <v-layout justify-center align-content-center>
+      <!-- <v-layout row wrap> -->
+      <v-flex lg6 xs6>
+        <v-card color="green" dark>
+          <v-card-text v-text="lorem">
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <!-- <v-flex lg6 xs6>
+              <v-spacer></v-spacer>
+            </v-flex> -->
+    <v-layout justify-center align-content-center>
+      <v-flex lg6 xs6>
+        <!-- <v-layout justify-center> -->
+        <v-data-table v-bind:headers="headers" v-bind:items="items" v-bind:search="search" hide-actions>
+          <template slot="items" scope="props">
+            <td class="text-xs-right">{{ props.item.id }}</td>
+            <td>
+              <v-edit-dialog lazy> {{ props.item.filename }}
+                <v-text-field slot="input" label="Edit" v-model="props.item.name" single-line counter :rules="[max25chars]"></v-text-field>
+              </v-edit-dialog>
+            </td>
+
+            <td class="text-xs-right">{{ props.item.uploadby }}</td>
+            <td class="text-xs-right">{{ props.item.datecreated }}</td>
+            <td class="text-xs-right">{{ props.item.size }}</td>
+            <td class="text-xs-right">{{ props.item.link }}</td>
+            <td class="text-xs-right">
+              <!-- <v-edit-dialog @open="tmp = props.item.iron" @save="props.item.iron = tmp || props.item.iron" large lazy>
+                                              <div>{{ props.item.iron }}</div>
+                                              <div slot="input" class="mt-3 title">Update Iron</div>
+                                              <v-text-field slot="input" label="Edit" v-model="tmp" single-line counter autofocus :rules="[max25chars]"></v-text-field>
+                                          </v-edit-dialog> -->
+            </td>
+          </template>
+          <!-- <template slot="pageText" scope="{ pageStart, pageStop }">
+                                      From {{ pageStart }} to {{ pageStop }}
+                                  </template> -->
+        </v-data-table>
+        <!-- </v-layout> -->
+      </v-flex>
+      <!-- </v-layout> -->
+    </v-layout>
+  </v-container>
 </template>
 <script>
-  export default {
-    data: () => ({
-      lorem: `List download file`
-    })
-  }
+export default {
+  data: () => ({
+    lorem: `List download file`,
+    max25chars: (v) => v.length <= 25 || 'Input too long!',
+    tmp: '',
+    search: '',
+    pagination: {
+    },
+    pages: 0,
+    headers: [
+      { text: 'ID', value: 'id' },
+      {
+        text: 'Filename',
+        align: 'left',
+        sortable: false,
+        value: 'name'
+      },
+      { text: 'Upload By', value: 'fat' },
+      { text: 'Date Create', value: 'carbs' },
+      { text: 'Size', value: 'protein' },
+      { text: 'Action', value: 'sodium' },
+    ],
+    items: []
+  })
+}
 </script>
--- src/components/UploadPersons.vue
+++ src/components/UploadPersons.vue
@@ -1,31 +1,37 @@
 <template>
     <v-container fuild>
-        <v-snackbar :top="y === 'top'" :timeout="timeout" :color="color" :vertical="mode === 'vertical'" v-model="snackbar">
-            {{ text }}
-            <v-btn dark flat @click.native="snackbar = false; clearErrUsr(), clearErrPasswd()">OK</v-btn>
-        </v-snackbar>
-        <v-layout row wrap>
-            <!-- <v-flex xs4>
-                                                                <v-subheader>Normal with hint text</v-subheader>
-                                                            </v-flex> -->
-            <v-form v-model="valid">
-                <v-flex lg 12 xs12 sm5>
-                    <v-text-field name="input-1-3" label="this is file location" v-model="file.name" single-line prepend-icon="attachment"></v-text-field>
-                </v-flex>
-                <v-flex lg 12 xs12 sm5>
-                    <input id="anu" type="file" ref="file" v-on:change="readfileloc">
-                    <v-btn :loading="loading3" @click.native="loader = 'loading3'" @click="upload" :disabled="loading3" color="blue-grey" dark>
-                        Upload
-                        <v-icon right dark>cloud_upload</v-icon>
-                        <!-- <input type="file" @change="onFileChange" color="blue-grey" dark> -->
-                    </v-btn>
-                </v-flex>
-            </v-form>
-        </v-layout>
+        <v-layout justify-center align-content-center>
+            <v-snackbar :top="y === 'top'" :timeout="timeout" :color="color" :vertical="mode === 'vertical'" v-model="snackbar">
+                {{ text }}
+                <v-btn dark flat @click.native="snackbar = false">OK</v-btn>
+            </v-snackbar>
+            <v-flex lg1 sm1>
+                <v-layout row wrap>
+                    <v-form ref="form">
+                        <v-flex lg12 sm12>
 
+                            <input id="infile" type="file" ref="file" v-on:change="readfileloc">
+
+                        </v-flex>
+                        <!-- <v-flex lg8 sm8>
+                                                    <v-spacer>
+                                                    </v-spacer>
+                                                </v-flex> -->
+                        <v-flex lg12 sm12>
+                            <v-btn :loading="loading3" @click.native="loader = 'loading3'" @click="upload" :disabled="loading3" color="blue-grey" dark>
+                                Upload
+                                <v-icon right dark>cloud_upload</v-icon>
+                                <!-- <input type="file" @change="onFileChange" color="blue-grey" dark> -->
+                            </v-btn>
+                        </v-flex>
+                    </v-form>
+                </v-layout>
+            </v-flex>
+        </v-layout>
     </v-container>
 </template>
 <script>
+import router from '@/router/index.js'
 import { HTTP } from '@/router/index'
 
 export default {
@@ -34,36 +40,99 @@ export default {
         loading3: false,
         file: ``,
         snackbar: false,
-            y: 'top',
-            x: null,
-            color: 'red',
-            mode: '',
-            timeout: 6000,
-            text: 'Ups.. upload cannot more than 5MB',
+        y: 'top',
+        x: null,
+        color: 'red',
+        mode: '',
+        timeout: 6000,
+        text: '',
     }),
+    computed: {
+        token: function() {
+            return this.$store.getters.getToken
+        },
+        isauth: function() {
+            return this.$store.getters.getIsAuth
+        },
+        islogin: function() {
+            return this.$store.getters.getIsLogin
+        }
+    },
     methods: {
         readfileloc: function(e) {
-            var file = (document.getElementById("anu").files[0])
+            var file = (document.getElementById("infile").files[0])
             this.file = file.name
             console.log(this.file)
         },
         upload: function() {
             console.log("lagi upload gaes")
-            var file = (document.getElementById("anu").files[0])
+            var file = (document.getElementById("infile").files[0])
             console.log(file)
-            const config = { headers: { 'Content-Type': 'multipart/form-data' } };
-            // this is a boundary file, without it will be error :)
-            let fd = new FormData();
-            fd.append('file',file)
-            HTTP.post(`/uploadfile`,fd, config)
+            if (file != null) {
+                const config = {
+                    withCredentials: true,
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                        'Authorization': 'Bearer ' + this.token
+                    }
+                };
+                // this is a boundary file, without it will be error :)
+                let fd = new FormData();
+                fd.append('file', file)
+                HTTP.post(`/uploadfile`, fd, config)
+                    .then(response => {
+                        // JSON responses are automatically parsed.
+                        console.log(response.status)
+                        if (response.status == 200) {
+                            this.text = "File uploaded successfuly.."
+                            this.color = "green"
+                            this.snackbar = true
+                            document.getElementById("infile").value = "";
+                        }
+
+                    })
+                    .catch(e => {
+                        console.log(e)
+                        if (e.response.status == 400) {
+                            this.color = "red"
+                            this.text = "Oops.. upload size can't more than 1MB"
+                            this.snackbar = true
+                        }
+
+                    })
+            } else {
+                this.color = "red"
+                this.text = "pilih file yang akan di upload"
+                this.snackbar = true
+            }
+        },
+        clear() {
+            this.$refs.form.reset()
+        }
+    },
+    created: function() {
+        console.log('token di admin:' + this.token)
+        console.log('auth di admin:' + this.isauth)
+        console.log('login di admin:' + this.islogin)
+        if (this.isauth != true) {
+            // router.push({ name: 'user', params: { userId: 123 }})
+            router.push({ name: 'ErrorPage', params: { errnum: 401 } })
+
+        } else {
+            // TODO make request and check result for handle 401
+            HTTP.get(`/uploadfile`, {
+                withCredentials: true,
+                timeout: 1000,
+                headers: {
+                    'Authorization': 'Bearer ' + this.token,
+                }
+            })
                 .then(response => {
                     // JSON responses are automatically parsed.
-                    console.log(response.status)
-                    // this.clear()
+                    console.log(response)
                 })
                 .catch(e => {
                     console.log(e)
-                    this.snackbar = true
                 })
         }
     }
